553e96d55b7554dd78e505de78363712e2a897ac
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-15 08:46:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
+    arr = []
     params[:number].to_i.times do
-      puts params[:phrase]
-    end.to_s
+      arr << params[:phrase]
+    end
+    arr
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
