eb5065c87e81304f35ba005a5d7373ae24131050
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-15 08:59:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,6 +23,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    fail
     result = ''
     if params[:operation] == 'add'
       result = (params[:number1].to_i + params[:number1].to_i).to_s
